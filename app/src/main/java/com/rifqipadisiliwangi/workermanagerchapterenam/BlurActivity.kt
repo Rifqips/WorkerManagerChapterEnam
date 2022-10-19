0e9f3d468d648f0a7c8a07d1e4d2302185f2e8ec
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-10-20 00:57:29 +0700

    Initial commit

---
+++ app/src/main/java/com/rifqipadisiliwangi/workermanagerchapterenam/BlurActivity.kt
@@ -0,0 +1,33 @@
+package com.rifqipadisiliwangi.workermanagerchapterenam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.rifqipadisiliwangi.workermanagerchapterenam.databinding.ActivityBlurBinding
+
+class BlurActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityBlurBinding
+    private val viewModel: BlurViewModel by viewModels { BlurViewModelFactory(application) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =  ActivityBlurBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goButton.setOnClickListener {
+            viewModel.applyBlur(blurLevel)
+
+        }
+    }
+
+    private val blurLevel :Int
+    get() =
+        when (binding.radioBlurGroup.checkedRadioButtonId){
+            R.id.radio_blur_lv_1 -> 1
+            R.id.radio_blur_lv_2 -> 2
+            R.id.radio_blur_lv_3 -> 3
+
+            else -> 1
+
+        }
+}
